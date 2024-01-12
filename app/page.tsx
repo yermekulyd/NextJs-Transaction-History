a0388105c33ee6041b0cf6175411438a38eda061
Didar Yermekuly <yermekulyd@gmail.com>
2024-01-13 03:32:53 +0600

    [edit] styling the main page, font-size, adding buttons and etc.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-2xl font-bold">Asadal Pay Test Task</h1>
-      <Link href="/transactions">Transactions</Link>
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold mb-6">Welcome to Didar's Test Task</h1>
+      <p className="text-xl mb-4">Explore your Transaction History</p>
+      <Link
+        href="/transactions"
+        className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition-colors"
+      >
+        View Transactions
+      </Link>
     </main>
   );
 }
